39af7268c3aee2b9f9f74e9c0820819a53f4e87a
Abner Yang <abtyang@gmail.com>
2017-11-16 01:10:28 -0500

    #4 added user roles - artist & artist employer

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  enum role: { musician: 0,artist_employer: 1 }
 end
