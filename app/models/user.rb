c951d40f12dfdd51caec535b97dda0ea4dc9fba0
Abner Yang <abtyang@gmail.com>
2017-11-29 18:29:22 -0500

    #4 added basic fb oauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,26 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-  enum role: { musician: 0,artist_employer: 1 }
+	# Include default devise modules. Others available are:
+	# :confirmable, :lockable, :timeoutable and :omniauthable
+	devise :database_authenticatable, :registerable,
+	:recoverable, :rememberable, :trackable, :validatable,
+	:omniauthable, :omniauth_providers => [:facebook]
+
+	enum role: { musician: 0, artist_employer: 1 }
+
+	def self.from_omniauth(auth)
+		where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+			user.email = auth.info.email
+			user.password = Devise.friendly_token[0,20]
+			parse_name(user, auth.info.name)
+			user.role = auth.user_role
+		end
+	end
+
+	private
+	def self.parse_name(user, name)
+		name_arr = name.split(" ")
+		user.last_name = name_arr.pop
+		user.first_name = name_arr.join(" ")
+	end
 end
+	
