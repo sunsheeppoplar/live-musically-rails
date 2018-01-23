4b41a304412c4e4e4a32db63f3e40d947fab0be6
Abner Yang <abtyang@gmail.com>
2018-01-23 16:46:05 -0500

    #14 added method to check that event end time isn't before start time

--- app/forms/employer_opportunity_form.rb
+++ app/forms/employer_opportunity_form.rb
@@ -20,6 +20,7 @@ class EmployerOpportunityForm
 
 	# validates :title, presence: true
 	# validates :category, presence: true
+	validate :is_timerange_valid?
 
 	def save
 		if valid?
@@ -30,14 +31,35 @@ class EmployerOpportunityForm
 		end
 	end
 
+	def is_timerange_valid?
+		@time_service = ConfigureTimeService.new(event_params)
+		@time_service.convert
+		if @time_service.is_end_time_before_start_time?
+			errors.add(:event_end_time, "can't be before start time")
+		end
+	end
+
 	private
 	def persist!
-		time_service = ConfigureTimeService.new(event_params).convert
-		opportunity = Opportunity.create!(artist_type: artist_type, description: description, event_end_date: time_service[:full_start_date], event_start_date: time_service[:full_end_date], title: title)
-		@venue = opportunity.create_venue!(address: address, category: category, city: city, name: name, zip: zip)
+		binding.pry
+		opportunity = Opportunity.create!(
+			artist_type: artist_type,
+			description: description,
+			event_end_date: @time_service[:full_date_hash][:full_start_date],
+			event_start_date: @time_service[:full_date_hash][:full_end_date],
+			title: title)
+		@venue = opportunity.create_venue!(
+			address: address,
+			category: category,
+			city: city,
+			name: name,
+			zip: zip)
 	end
 
 	def event_params
-		{ event_end_date: event_end_date, event_end_time: event_end_time, event_start_date: event_start_date, event_start_time: event_start_time }
+		{ 	event_end_date: event_end_date,
+			event_end_time: event_end_time,
+			event_start_date: event_start_date,
+			event_start_time: event_start_time }
 	end
 end
--- app/services/configure_time_service.rb
+++ app/services/configure_time_service.rb
@@ -6,11 +6,20 @@ class ConfigureTimeService
 	attribute :event_start_date, DateTime
 	attribute :event_start_time, String
 
+	attr_reader :full_date_hash
+
 	def convert
 		@start_date_object = convert_date_string(event_start_date)
 		@end_date_object = convert_date_string(event_end_date)
 
 		return_hash
+	end
+
+	def is_end_time_before_start_time?
+		start_time = datetime_to_integer(@full_date_hash[:full_start_date])
+		end_time = datetime_to_integer(@full_date_hash[:full_end_date])
+
+		end_time < start_time
 	end
 
 	private
@@ -20,15 +29,21 @@ class ConfigureTimeService
 		end
 	end
 
+
 	def return_hash
-		{
-			full_start_date: set_date(@start_date_object, event_start_time),
-			full_end_date: set_date(@end_date_object, event_end_time)
+		@full_date_hash = {
+			full_start_date: set_date(@start_date_object,event_start_time),
+			full_end_date: set_date(@end_date_object,
+				event_end_time)
 		}
 	end
 
 	def set_date(date, time)
 		converted_time = Time.parse(time)
 		date.change(hour: converted_time.hour, min: converted_time.min)
+	end
+
+	def datetime_to_integer(datetime_object)
+		datetime_object.strftime("%Y%m%d%H%M").to_i
 	end
 end
