daceedb117239f65f27e3d7d87293f547aaa116b
Abner Yang <abtyang@gmail.com>
2018-01-26 12:41:30 -0500

    #14 fixed typo for checkbox selection logic

--- app/assets/javascripts/new-opportunity-form.js
+++ app/assets/javascripts/new-opportunity-form.js
@@ -1,6 +1,6 @@
 $(document).on('turbolinks:load', function() {
 	$('.js-new-opp-form-timeframe-checkbox').on('change', function() {
-		$('.js-new-opp-formtimeframe-checkbox').not(this).prop('checked', false);
+		$('.js-new-opp-form-timeframe-checkbox').not(this).prop('checked', false);
 	})
 
 	$('.js-new-opp-form-venue-checkbox').on('change', function() {
