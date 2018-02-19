d95020025d11b643496d518a20ce7c027aeca20d
Abner Yang <abtyang@gmail.com>
2018-02-19 18:22:36 -0500

    #26 refined query so that queries don't show expired posts

--- app/controllers/search/opportunities_controller.rb
+++ app/controllers/search/opportunities_controller.rb
@@ -5,7 +5,7 @@ class Search::OpportunitiesController < ApplicationController
 
 	def index
 		@opportunities_search_form = OpportunitiesSearchForm.new(opportunities_search_params)
-		query_base = Opportunity.includes(:artist_opportunities, :employer, :venue)
+		query_base = Opportunity.includes(:artist_opportunities, :employer, :venue).where('timeframe_of_post >= ?', DateTime.now)
 		basic_opportunities_query = BasicOpportunitiesQuery.new(scope:  query_base, params: @opportunities_search_form).call
 		@opportunity_query_decorator = OpportunityDecorator.wrap(basic_opportunities_query)
 	end
