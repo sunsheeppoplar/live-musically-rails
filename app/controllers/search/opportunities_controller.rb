0724e5196821a0bbd0b71cf00b222d594efdff21
Abner Yang <abtyang@gmail.com>
2018-02-20 18:52:45 -0500

    #26 changing over to module syntax for namespacing

--- app/controllers/search/opportunities_controller.rb
+++ app/controllers/search/opportunities_controller.rb
@@ -1,20 +1,22 @@
-class Search::OpportunitiesController < ApplicationController
-	def new
-		@opportunities_search_form = OpportunitiesSearchForm.new
-	end
+module Search
+	class OpportunitiesController < ApplicationController
+		def new
+			@opportunities_search_form = OpportunitiesSearchForm.new
+		end
 
-	def index
-		@opportunities_search_form = OpportunitiesSearchForm.new(opportunities_search_params)
+		def index
+			@opportunities_search_form = OpportunitiesSearchForm.new(opportunities_search_params)
 
-		query_base = Opportunity.includes(:artist_opportunities, :employer, :venue).where('timeframe_of_post >= ?', DateTime.now)
-		basic_opportunities_query = BasicOpportunitiesQuery.new(scope: query_base, params: @opportunities_search_form).call
-			.by_date
+			query_base = Opportunity.includes(:artist_opportunities, :employer, :venue).where('timeframe_of_post >= ?', DateTime.now)
+			basic_opportunities_query = BasicOpportunitiesQuery.new(scope: query_base, params: @opportunities_search_form).call
+				.by_date
 
-		@opportunity_query_decorator = OpportunityDecorator.wrap(basic_opportunities_query)
-	end
+			@opportunity_query_decorator = OpportunityDecorator.wrap(basic_opportunities_query)
+		end
 
-	private
-	def opportunities_search_params
-		params.require(:opportunities_search_form).permit(:event_start_time, :event_start_date, :event_end_time, :event_end_date, :instrument, :location)
+		private
+		def opportunities_search_params
+			params.require(:opportunities_search_form).permit(:event_start_time, :event_start_date, :event_end_time, :event_end_date, :instrument, :location)
+		end
 	end
 end
