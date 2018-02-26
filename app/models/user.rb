f5e5281f2dd640b68fbe08b583ae89d53abe4f65
nnouns <lokaiya@gmail.com>
2018-02-26 17:37:54 -0500

    continued js reorganization and playlist validation
    renamed remaining jq onload behavior in autocomplete.js to my-profile-init.js
    added validation to disallow duplicate playlist additions which should fix weird delete behavior

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,19 +10,19 @@ class User < ApplicationRecord
 	has_many :employing_opportunities, :class_name => 'Opportunity', :foreign_key => 'employer_id'
 	has_many :artist_opportunities
 	has_many :performing_opportunities, :through => :artist_opportunities, source: :opportunity
-    has_many :instruments, :through => :artist_instruments
-	has_many :artist_instruments
-    has_many :locations, :through => :artist_locations
-    has_many :artist_locations
-    has_many :external_links
+  has_many :instruments, :through => :artist_instruments
+  has_many :artist_instruments
+  has_many :locations, :through => :artist_locations
+  has_many :artist_locations
+  has_many :external_links
 
-    has_attached_file   :avatar, 
-                        :styles => {
-                            normal: ["250x250#", :png],
-                            small: ["215x215#", :png],
-                            thumb: ["50x50#", :png]
-                        }
-    validates_attachment_content_type :avatar, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+  has_attached_file   :avatar,
+                      :styles => {
+                          normal: ["250x250#", :png],
+                          small: ["215x215#", :png],
+                          thumb: ["50x50#", :png]
+                      }
+  validates_attachment_content_type :avatar, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 
 
 	def self.from_omniauth(auth)
