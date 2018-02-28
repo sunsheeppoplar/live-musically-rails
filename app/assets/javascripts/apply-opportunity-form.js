44daf50d45e0c536b6624ce39ac96f181985fb6c
Abner Yang <abtyang@gmail.com>
2018-02-28 15:48:53 -0500

    #26 changed form to be viewable as first node of body, wip, still need to style and position

--- app/assets/javascripts/apply-opportunity-form.js
+++ app/assets/javascripts/apply-opportunity-form.js
@@ -1,5 +1,5 @@
 $(document).on('turbolinks:load', function() {
 	$('.js-apply-opp-form__open-modal-button').on('ajax:success', function(e, data, status, xhr) {
-		$('body').append(data.new_application)
+		$('body').prepend(data.new_application)
 	}) 
 })
