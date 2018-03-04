834b4b832219351cae7f45fed0777390a9ae882d
nnouns <lokaiya@gmail.com>
2018-03-04 15:33:41 -0500

    added migrations and models for Conversations and Messages

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180216223707) do
+ActiveRecord::Schema.define(version: 20180304195454) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,13 @@ ActiveRecord::Schema.define(version: 20180216223707) do
     t.index ["user_id"], name: "index_artist_opportunities_on_user_id", using: :btree
   end
 
+  create_table "conversations", force: :cascade do |t|
+    t.integer  "sender_id"
+    t.integer  "recipient_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
   create_table "external_links", force: :cascade do |t|
     t.integer "user_id"
     t.string  "origin_site"
@@ -60,6 +67,17 @@ ActiveRecord::Schema.define(version: 20180216223707) do
     t.string "long"
     t.string "world_region"
     t.string "country"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text     "body"
+    t.integer  "conversation_id"
+    t.integer  "user_id"
+    t.boolean  "read",            default: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+    t.index ["conversation_id"], name: "index_messages_on_conversation_id", using: :btree
+    t.index ["user_id"], name: "index_messages_on_user_id", using: :btree
   end
 
   create_table "opportunities", force: :cascade do |t|
