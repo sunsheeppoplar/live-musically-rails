5f0c383cfa3fa7f0e18380a102d5eea7851ea731
Abner Yang <abtyang@gmail.com>
2018-03-09 20:51:11 -0500

    closes #27

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,68 +3,55 @@ class ProfilesController < ApplicationController
 	
 	def my_profile
 		layout = find_user_role(current_user.role)
-        @my_profile_form = MyProfileForm.new(current_user: current_user)
-        respond_to do |format|
-            format.html { render layout }
-            format.json { render json: 
-                { 
-                    instruments: current_user.instruments, 
-                    locations: current_user.locations, 
-                    ext_links: current_user.external_links
-                } 
-            }
-        end
+		@my_profile_form = MyProfileForm.new(current_user: current_user)
+		respond_to do |format|
+			format.html { render layout }
+			format.json { render json:
+				{
+					instruments: current_user.instruments,
+					locations: current_user.locations,
+					ext_links: current_user.external_links
+				}
+			}
+		end
 	end
 
-    def update
-        @my_profile_form = MyProfileForm.new(profile_form_params)
+	def update
+		@my_profile_form = MyProfileForm.new(profile_form_params)
 		respond_to do |format|
-            if @my_profile_form.update
-                # flash[:notice] = 'Updated'
-                format.html { redirect_to my_profile_path, notice: 'Updated'
-                }
-                # prevents UndefinedConversionError
-                @my_profile_form.avatar = {}
+			if @my_profile_form.update
+				if @my_profile_form.updating_password?
+					bypass_sign_in(@my_profile_form.current_user)
+				end
+				# flash[:notice] = 'Updated'
+				format.html { redirect_to my_profile_path, notice: 'Updated'
+				}
+				# prevents UndefinedConversionError
+				@my_profile_form.avatar = {}
 				format.json { render json: @my_profile_form
 				}
 			else
-                # flash[:alert]
+				# flash[:alert]
 				format.html { redirect_to my_profile_path }
 			end
 		end
-    end
-    
-    def get_single_zipcode
-        @location  = Location.where(zipcode: params[:zipcode])
-        respond_to do |format|
-            format.html { redirect_to my_profile_path, notice: 'Updated'
-            }
-            format.json { render json: { location: @location }
-            }
-        end
-    end
+	end
 
-	def update_password
-		@my_profile_form = MyProfileForm.new(profile_form_params)
-		@user = @my_profile_form.current_user
-
+	def get_single_zipcode
+		@location  = Location.where(zipcode: params[:zipcode])
 		respond_to do |format|
-			if @my_profile_form.update_password
-				bypass_sign_in(@user)
-				# flash[:notice] = 'Updated'
-				format.json { render json: { my_profile_form: @my_profile_form }
-				}
-				format.html { redirect_to my_profile_path, notice: 'Updated'}
-			else
-				# flash[:alert]
-				redirect_to my_profile_path
-			end
+			format.html { redirect_to my_profile_path, notice: 'Updated'
+			}
+			format.json { render json: { location: @location }
+			}
 		end
-    end
-    
+	end
+
 	private
 	def profile_form_params
-		params.require(:my_profile_form).permit(:about, :avatar, :email, :first_name, :last_name, :password, :password_confirmation, :instruments => [], :locations => [], :soundcloud_links => [], :youtube_links => []).merge(current_user: current_user)
+		params.require(:my_profile_form).permit(:about, :avatar, :email, :first_name, :last_name, :password, :password_confirmation, :instruments => [], :locations => [], :soundcloud_links => [], :youtube_links => [])
+			.merge(current_user: current_user)
+			.reject {|k, v| (k == "password_confirmation" || k == "password") && v.blank? }
 	end
 
 	def find_user_role(role)
