b2bed6491d92885e06a3350e8a794c1dd4c70b90
Abner Yang <abtyang@gmail.com>
2018-03-10 19:40:22 -0500

    #19 corrected broken method names

--- app/controllers/opportunities_controller.rb
+++ app/controllers/opportunities_controller.rb
@@ -3,9 +3,9 @@ class OpportunitiesController < ApplicationController
 	before_action :set_opportunity, only: [:edit, :destroy]
 
 	def new
-		if opportunity_policy.fully_onboarded?
+		if opportunity_policy.fully_onboarded_employer?
 			@employer_opportunity_form = EmployerOpportunityForm.new
-		elsif opportunity_policy.partially_onboarded?
+		elsif opportunity_policy.partially_onboarded_employer?
 			redirect_to onboard_path, notice: "Please complete Stripe registration before posting an opportunity"
 		else
 			redirect_to root_path, alert: "Not authorized, sorry!"
