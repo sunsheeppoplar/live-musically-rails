e83514e09e7bb7cd81e9aa9623f2258ecf29f589
Abner Yang <abtyang@gmail.com>
2018-05-02 01:37:58 -0400

    Add Staging Configuration For Slack And Stripe
    - #35

--- config/initializers/slack.rb
+++ config/initializers/slack.rb
@@ -1,4 +1,7 @@
-SLACK_NOTIFIER = Slack::Notifier.new(Rails.application.secrets.slack_webhook_url) do
-	defaults 	channel: "#stripe-payments",
-				username: "Stripe Notifier"
+unless Rails.env.test?
+	slack_webhook_url = ENV["SLACK_WEBHOOK_URL"] || Rails.application.secrets.slack_webhook_url
+	SLACK_NOTIFIER = Slack::Notifier.new(slack_webhook_url) do
+		defaults 	channel: "#stripe-payments",
+					username: "Stripe Notifier"
+	end
 end
--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,4 +1,6 @@
-Stripe.api_key = Rails.application.secrets.stripe_oauth_app_secret
-STRIPE_PUBLISHABLE_KEY = Rails.application.secrets.stripe_publishable_key
-STRIPE_YEARLY_SUBSCRIPTION_ID = Rails.application.secrets.stripe_subscription_yearly_id
-STRIPE_MONTHLY_SUBSCRIPTION_ID = Rails.application.secrets.stripe_subscription_monthly_id
+Stripe.api_key = Rails.env.production? ? ENV[""] : Rails.application.secrets.stripe_oauth_app_secret
+Stripe.max_network_retries = 3
+
+STRIPE_PUBLISHABLE_KEY = Rails.env.production? ? ENV["STRIPE_PUBLISHABLE_KEY"] : Rails.application.secrets.stripe_publishable_key
+STRIPE_YEARLY_SUBSCRIPTION_ID = Rails.env.production? ? ENV["STRIPE_YEARLY_SUBSCRIPTION_ID"] : Rails.application.secrets.stripe_subscription_yearly_id
+STRIPE_MONTHLY_SUBSCRIPTION_ID = Rails.env.production? ? ENV["STRIPE_MONTHLY_SUBSCRIPTION_ID"] : Rails.application.secrets.stripe_subscription_monthly_id
