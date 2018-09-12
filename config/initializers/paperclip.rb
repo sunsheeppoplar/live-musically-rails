b14a250c8b7a893eb73c0b4417a2f2150507b8c0
Abner Yang <abtyang@gmail.com>
2018-09-12 16:58:31 -0400

    Adds AWS Staging Keys for Heroku
    - moves config from production.rb file into separate paperclip initializer
    - gives us more flexibility for future changes
    - closes #35

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,1 +1,15 @@
 Paperclip::DataUriAdapter.register
+
+AWS_ACCESS_KEY_ID = Rails.env.production? ENV["AWS_ACCESS_KEY_ID"] : Rails.application.secrets.aws_access_key_id
+AWS_SECRET_ACCESS_KEY = Rails.env.production? ENV["AWS_SECRET_ACCESS_KEY"] : Rails.application.secrets.aws_secret_access_key
+
+Paperclip.options[:command_path] = "/usr/local/bin/"
+config.paperclip_defaults = {
+storage: :s3,
+s3_credentials: {
+  bucket: "live-musically-dev",
+  access_key_id: AWS_ACCESS_KEY_ID,
+  secret_access_key: AWS_SECRET_ACCESS_KEY,
+  s3_region: "us-east-1"
+}
+}
