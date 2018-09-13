debeb3ab7f741769d98beaf8a5c7410823ac612a
Abner Yang <abtyang@gmail.com>
2018-09-12 20:54:27 -0400

    Another Attempt To Consolidate Paperclip Config
    - found related issue https://github.com/thoughtbot/paperclip/issues/2240
    - #35

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,1 +1,15 @@
 Paperclip::DataUriAdapter.register
+
+AWS_ACCESS_KEY_ID = Rails.env.production? ? ENV["AWS_ACCESS_KEY_ID"] : Rails.application.secrets.aws_access_key_id
+AWS_SECRET_ACCESS_KEY = Rails.env.production? ? ENV["AWS_SECRET_ACCESS_KEY"] : Rails.application.secrets.aws_secret_access_key
+
+Paperclip::Attachment.default_options.update({	
+	storage: :s3,
+	s3_credentials: {
+		bucket: "live-musically-dev",
+		access_key_id: AWS_ACCESS_KEY_ID,
+		secret_access_key: AWS_SECRET_ACCESS_KEY,
+		s3_host_name: "s3.amazonaws.com",
+		s3_region: "us-east-1"
+	}
+})
